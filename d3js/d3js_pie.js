7c59d2d6cc880e4537dd251fe00d31190c0ba335
Choi Yein <yeiny1004@naver.com>
2018-04-03 19:40:49 +0900

    pie 수정

--- d3js/d3js_pie.js
+++ d3js/d3js_pie.js
@@ -1,12 +1,17 @@
-var svgWidth = 320;
-var svgHeight = 240;
+var svgWidth = 600;
+var svgHeight = 600;
 var dataSet = [21, 42, 20, 11, 7];
 
 var pie = d3.layout.pie();
 
-var arc = d3.svg.arc().innerRadius(0).outerRadius(100);
+var arc = d3.svg.arc().innerRadius(0).outerRadius(220);
 
 var pieElements = d3.select("#myGraph")
+    // .selectAll("g")
+    // .data(pie(dataSet))
+    // .enter()
+    // .append("g")
+    // .attr("transform", "translate("+svgWidth/2+", "+svgHeight/2+")")
     .selectAll("path")
     .data(pie(dataSet))
 
@@ -16,5 +21,6 @@ pieElements.enter()
     .attr("d", arc)
     .attr("transform", "translate("+svgWidth/2+", "+svgHeight/2+")")
     .style("fill", function(d,i){
-        return ["red", "orange", "yellow", "cyan", "#3f3"][i];
+        return ["rgba(242, 231, 84, 0.5)", "rgba(81, 176, 232, 0.5)","rgba(48, 173, 69, 0.5)","rgba(219, 50, 50, 0.5)","rgba(174, 67, 232, 0.5)"][i];
     })
+
