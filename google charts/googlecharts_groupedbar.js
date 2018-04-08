e73405e776f40be00fd630b7687f4c64887d9df0
Choi Yein <yeiny1004@naver.com>
2018-04-08 19:33:45 +0900

    googlecharts_groupedbar

--- google charts/googlecharts_groupedbar.js
+++ google charts/googlecharts_groupedbar.js
@@ -1,42 +1,26 @@
-google.charts.load('current', {packages: ['corechart', 'bar']});
-google.charts.setOnLoadCallback(drawMultSeries);
+google.charts.load('current', {packages: ['corechart']});     
 
-function drawMultSeries() {
-      var data = new google.visualization.DataTable();
-      data.addColumn('timeofday', 'Time of Day');
-      data.addColumn('number', 'Motivation Level');
-      data.addColumn('number', 'Energy Level');
+function drawChart() {
+    // Define the chart to be drawn.
+    var data = google.visualization.arrayToDataTable([
+       ['Age', 'Male', 'Female'],
+       ['0s',  424685, 401302],
+       ['10s',  599453, 547056],
+       ['20s',  742981, 745963],
+       ['30s',  910258, 891984],
+       ['40s',  870688, 865091],
+       ['50s',  763881, 812455],
+       ['60s',  438938, 476794],
+       ['70s',  234147, 285892],
+       ['80s',  48952, 106184],
+       ['90s',  6357, 18331],
+       ['100s',  996, 2930]
+    ]);
 
-      data.addRows([
-        [{v: [8, 0, 0], f: '8 am'}, 1, .25],
-        [{v: [9, 0, 0], f: '9 am'}, 2, .5],
-        [{v: [10, 0, 0], f:'10 am'}, 3, 1],
-        [{v: [11, 0, 0], f: '11 am'}, 4, 2.25],
-        [{v: [12, 0, 0], f: '12 pm'}, 5, 2.25],
-        [{v: [13, 0, 0], f: '1 pm'}, 6, 3],
-        [{v: [14, 0, 0], f: '2 pm'}, 7, 4],
-        [{v: [15, 0, 0], f: '3 pm'}, 8, 5.25],
-        [{v: [16, 0, 0], f: '4 pm'}, 9, 7.5],
-        [{v: [17, 0, 0], f: '5 pm'}, 10, 10],
-      ]);
+    var options = {title: 'Populations per gender in 2012 Seoul'};  
 
-      var options = {
-        title: 'Motivation and Energy Level Throughout the Day',
-        hAxis: {
-          title: 'Time of Day',
-          format: 'h:mm a',
-          viewWindow: {
-            min: [7, 30, 0],
-            max: [17, 30, 0]
-          }
-        },
-        vAxis: {
-          title: 'Rating (scale of 1-10)'
-        }
-      };
-
-      var chart = new google.visualization.ColumnChart(
-        document.getElementById('chart_div'));
-
-      chart.draw(data, options);
-    }
+    // Instantiate and draw the chart.
+    var chart = new google.visualization.ColumnChart(document.getElementById('container'));
+    chart.draw(data, options);
+ }
+ google.charts.setOnLoadCallback(drawChart);
