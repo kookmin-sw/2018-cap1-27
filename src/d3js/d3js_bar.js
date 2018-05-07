1470a718bbdd5151d1f0f1d59da3403385b4b6b3
Choi Yein <yeiny1004@naver.com>
2018-05-07 21:07:10 +0900

    막대그래프 수정
    순차적 애니메이션 효과 추가, 마우스오버 효과 추가, 전체적인 그래프 길이 늘이기
    (축 범위 수정해야함)

--- src/d3js/d3js_bar.js
+++ src/d3js/d3js_bar.js
@@ -1,12 +1,12 @@
-var svgWidth = 900;
-var svgHeight = 400;
-var offsetX = 30;
-var offsetY = 35;
+var svgWidth = 900;  //320
+var svgHeight = 400;  //240
+var offsetX = 30;  //45
+var offsetY = 35;  //10
 var barElements;
 var dataSet = [-2.4, 0.4, 5.7, 12.5, 17.8, 22.2, 24.9, 25.7, 21.2, 14.8, 7.2, 0.4];
-var dataMax = 300;
-var barWidth = 20;
-var barMargin = 50;
+var dataMax = 300;  //300
+var barWidth = 20;  //20
+var barMargin = 50;  //5
 
 barElements = d3.select("#myGraph")
             .selectAll("rect")
@@ -16,15 +16,37 @@ barElements = d3.select("#myGraph")
             .append("rect")
             .attr("class", "bar")
             .attr("height", function(d,i){
-                return d;
+                return 0;
             })
             .attr("width", barWidth)
             .attr("x", function(d,i){
                 return i*(barWidth+barMargin) + offsetX;
             })
+            // .attr("y", function(d,i){
+            //     return svgHeight-d-offsetY;
+            // }) 
+            .attr("y", svgHeight-offsetY) 
+            
+            .on("mouseover", function(){
+                d3.select(this)
+                .style("fill", "rgb(235,55,85)")
+            })
+            .on("mouseout", function(){
+                d3.select(this)
+                .style("fill", "rgba(226, 38, 38, 0.2)")
+            })
+            
+            .transition()
+            .duration(1000)
+            .delay(function(d,i){
+                return i*100;
+            })
             .attr("y", function(d,i){
-                return svgHeight-d-offsetY;
-            })          
+                return svgHeight-d*10-offsetY;
+            })
+            .attr("height", function(d,i){
+                return d*10;
+            })
            
         barElements.enter()
             .append("text")
